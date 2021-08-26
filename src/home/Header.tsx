ee407023102e58b3d540fff8ac47f4403db5fc65
Mahbubur Rahman <rifat0153@gmail.com>
2021-08-27 01:21:01 +0600

    Mobile Header Done

--- src/home/Header.tsx
+++ src/home/Header.tsx
@@ -1,7 +1,6 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { ReactElement, useState } from 'react';
 import { useEffect } from 'react';
-import { faCoffee } from '@fortawesome/fontawesome-free-solid'
+import { List } from 'react-bootstrap-icons';
 
 interface Props {}
 
@@ -21,11 +20,36 @@ export default function Header({}: Props): ReactElement {
 const MobileHeader = () => {
   const [isOpen, setisOpen] = useState(false);
 
+  const toggle = () => {
+    setisOpen(!isOpen);
+  };
+
   return (
-    <div className="  h-50 justify-end px-8">
-        Mobile
-        <span className="fa fa-facebook"></span>
-       
+    <div className="bg-indigo-dark text-white">
+      {/* Always open Row with Menu Button */}
+      <div
+        className="flex items-center h-50 justify-end px-8 pt-2"
+        onClick={toggle}
+      >
+        <List className="block" size={40} />
+      </div>
+      {/* menu list */}
+      {isOpen ? (
+        <div
+          className={`flex-col transition  duration-1000 
+        ease-in-out ${
+          isOpen ? 'fadeIn' : 'fadeOut'
+        }  items-center justify-end text-right px-7 pt-2 pb-4 space-y-3`}
+        >
+          <div className="text-lg px-2">About</div>
+          <div className="text-lg px-2">Experience</div>
+          <div className=" text-lg px-2">Work</div>
+          <div className=" text-lg px-2">Contact</div>
+        </div>
+      ) : (
+        <div>
+        </div>
+      )}
     </div>
   );
 };
