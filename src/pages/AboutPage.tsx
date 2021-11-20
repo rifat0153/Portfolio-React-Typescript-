dadbbd998f427462d20d1e724387a7d49e54396e
Mahbubur Rahman <rifat0153@gmail.com>
2021-11-20 23:07:18 +0000

    About page  new layout INIT

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -5,17 +5,41 @@ interface Props {}
 export const AboutPage = (props: Props) => {
   return (
     <div className="bg-main-dark text-gray-200">
-      <main className="max-w-6xl mx-auto py-8">
-        <div className="grid grid-cols-2">
-          {/* Left Side */}
-          <div className="pl-2 bg-white">
-            <text className=" text-2xl">I am a Software Engineer. </text>
-          </div>
+      <main className="max-w-4xl mx-auto py-8">
+        <p className="text-center font-bold lg:text-4xl text-3xl">
+          About Mahbubur Rahman
+        </p>
 
-          {/* Right Side */}
+        <div className="grid grid-cols-2"></div>
+
+        <div className="py-8 text-2xl">
+          <p>
+            I am a Full Stack Developer with 1 year commercial experience and
+            graduate of Computer Science. I creates successful apps that are
+            fast, easy to use, and built with best practices.
+          </p>
+        </div>
+        <div className="grid grid-cols-2 space-x-2 ">
+          {/* First Row */}
+          <div className="pl-2 bg-gray-800">
+            <text className="text-2xl">I am a Software Engineer. </text>
+          </div>
           <div>
             <text>Right Side</text>
           </div>
+
+          {/* 2nd Row */}
+          <div className="bg-gray-800">
+            <text className="text-2xl">I am a Software Engineer. </text>
+          </div>
+          <div>
+            <text>Right Side</text>
+          </div>
+
+          <div className="text-4xl">Age</div>
+          <div className="text-3xl">26</div>
+
+          <div>5th Div</div>
         </div>
       </main>
     </div>
